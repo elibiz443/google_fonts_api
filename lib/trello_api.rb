56d87599b8fdd1eece12377f5a8e6e2284a982e5
elibiz443 <ellyambet10@gmail.com>
2022-09-02 04:32:24 +0300

    Added object to retrieve all cards from lists

--- lib/trello_api.rb
+++ lib/trello_api.rb
@@ -11,8 +11,22 @@ class TrelloApi
     response = HTTParty.get('https://api.trello.com/1/boards/1XXZ1ZKd/lists?key=' + Application.new.trello_key + '&token=' + Application.new.trello_token)
     response.collect{|data| data["name"]}
   end
+
+  def list_ids
+    response = HTTParty.get('https://api.trello.com/1/boards/1XXZ1ZKd/lists?key=' + Application.new.trello_key + '&token=' + Application.new.trello_token)
+    response.collect{|data| data["id"]}
+  end
+
+  def cards
+    list_ids.each do |list_id|
+      card_detail = HTTParty.get('https://api.trello.com/1/lists/' + list_id + '/cards?key=' + Application.new.trello_key + '&token=' + Application.new.trello_token)
+      puts card_detail.collect{|data| data["name"]}
+    end
+  end
 end
 
 output = TrelloApi.new
 # puts output.board_name
-puts output.lists
+# puts output.lists
+# puts output.list_ids
+output.cards
