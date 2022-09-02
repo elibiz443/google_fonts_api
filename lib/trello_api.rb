ac8ba031cb22bc24ae55468700149f3dfab97320
elibiz443 <ellyambet10@gmail.com>
2022-09-02 03:50:44 +0300

    Added object to retrieve board lists

--- lib/trello_api.rb
+++ lib/trello_api.rb
@@ -6,7 +6,13 @@ class TrelloApi
     response = HTTParty.get('https://api.trello.com/1/boards/1XXZ1ZKd/name?key=' + Application.new.trello_key + '&token=' + Application.new.trello_token)
     response["_value"]
   end
+
+  def lists
+    response = HTTParty.get('https://api.trello.com/1/boards/1XXZ1ZKd/lists?key=' + Application.new.trello_key + '&token=' + Application.new.trello_token)
+    response.collect{|data| data["name"]}
+  end
 end
 
 output = TrelloApi.new
-puts output.board_name
+# puts output.board_name
+puts output.lists
